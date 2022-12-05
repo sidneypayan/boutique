5bd96a262d66bdd49b12e897a4d230699e983482
sidney <etreailleurs@hotmail.fr>
2022-12-05 15:29:51 +0100

    adding google auth for next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,14 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import FacebookProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from '../../../lib/prisma'
 
 export const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
-		FacebookProvider({
-			clientId: process.env.FACEBOOK_ID,
-			clientSecret: process.env.FACEBOOK_SECRET,
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
 	theme: {
