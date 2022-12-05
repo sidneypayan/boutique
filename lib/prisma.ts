7c7717ce5c884e5df9a199459a2d5545fa7d50a0
sidney <etreailleurs@hotmail.fr>
2022-12-05 18:02:18 +0100

    refactoring prisma db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-	var prisma: PrismaClient | undefined
-}
+const prisma = global.prisma || new PrismaClient()
 
-export const prisma = global.prisma || new PrismaClient()
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+export default prisma
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,9 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../lib/prisma'
+import prisma from '../../../lib/prisma'
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-	const products = await prisma.product.findMany()
-	res.status(200).json(products)
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { method } = req
+
+	switch (method) {
+		case 'GET':
+			try {
+				const products = await prisma.product.findMany()
+				res.status(200).json(products)
+			} catch (error) {
+				console.error('Request error', e)
+				res.status(500).json({ error: 'Error fetching posts' })
+			}
+			break
+		default:
+			res.setHeader('Allow', ['GET'])
+			res.status(405).end(`Method ${method} Not Allowed`)
+			break
+	}
 }
 
 export default handler
