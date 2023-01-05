161fde89e36eee5db09a31fa7d1f77131c075988
sidney <etreailleurs@hotmail.fr>
2023-01-05 16:08:16 +0100

    refactoring prisma db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = global.prisma || new PrismaClient()
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient()
+} else {
+	let globalWithPrisma = global as typeof globalThis & {
+		prisma: PrismaClient
+	}
+	if (!globalWithPrisma.prisma) {
+		globalWithPrisma.prisma = new PrismaClient()
+	}
+	prisma = globalWithPrisma.prisma
+}
 
 export default prisma
